bd5b04c415475f1a6d06f62cf3fd570c41221b77
Jhass Theisen <jhasstheisen1@gmail.com>
2022-11-30 09:44:54 -0800

    finished statics

--- week-10/D3/practice-for-week-10-express-static-files-main/practice-for-week-10-express-static-files-main/server/app.js
+++ week-10/D3/practice-for-week-10-express-static-files-main/practice-for-week-10-express-static-files-main/server/app.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const app = express();
 // Your code here
+const path = require('path');
+
+// p1
+// app.use('/', express.static(path.join(__dirname,'assets')));
+
+// p2
+// app.use('/', express.static(path.join(__dirname, 'assets/scripts')));
+
+// p3
+// app.use('/stylesheets', express.static(path.join(__dirname, 'assets/css')));
 
 const port = 5000;
 app.listen(port, () => console.log('Server is listening on port', port));
