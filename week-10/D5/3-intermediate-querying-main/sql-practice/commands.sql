a6125a588465e1b98b81d125b16fa4210af0f8ee
Jhass Theisen <jhasstheisen1@gmail.com>
2022-12-02 10:14:36 -0800

    finished querys and bonus

---
+++ week-10/D5/3-intermediate-querying-main/sql-practice/commands.sql
@@ -0,0 +1,25 @@
+-- p1
+SELECT num_sold FROM albums WHERE num_sold >= 100000;
+
+-- p2
+SELECT year FROM albums WHERE year BETWEEN 2018 AND 2020;
+
+-- p3
+SELECT band_id FROM albums WHERE band_id IN (1,3,4);
+
+-- p4
+SELECT title FROM albums Where title LIKE 'The%';
+
+-- p5
+SELECT num_sold FROM albums WHERE num_sold
+ORDER BY num_sold DESC
+LIMIT 2;
+
+-- p6
+-- check order
+SELECT num_sold FROM albums WHERE num_sold
+ORDER BY num_sold DESC;
+-- get 3rd 4th
+SELECT num_sold FROM albums WHERE num_sold
+ORDER BY num_sold DESC
+LIMIT 2 OFFSET 2;
