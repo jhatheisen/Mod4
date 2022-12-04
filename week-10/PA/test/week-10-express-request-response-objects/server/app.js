f1cfca212d5f723ca1dee561111b87d214981a41
Jhass Theisen <jhasstheisen1@gmail.com>
2022-12-04 10:58:43 -0800

    test

---
+++ week-10/PA/test/week-10-express-request-response-objects/server/app.js
@@ -0,0 +1,9 @@
+// DO NOT EDIT - Initialize Express, handle JSON requests
+const express = require('express');
+const app = express();
+
+
+
+// DO NOT EDIT - Set port and listener
+const port = 5000;
+app.listen(port, () => console.log('Server is listening on port', port));
