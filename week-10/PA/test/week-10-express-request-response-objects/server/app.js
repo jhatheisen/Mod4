2678fbaa4e423c2b7ef046c3c687d34ccfa2751d
Jhass Theisen <jhasstheisen1@gmail.com>
2022-12-04 11:53:54 -0800

    testuing

--- week-10/PA/test/week-10-express-request-response-objects/server/app.js
+++ week-10/PA/test/week-10-express-request-response-objects/server/app.js
@@ -2,7 +2,16 @@
 const express = require('express');
 const app = express();
 
+app.post('/world', (req, res) => { console.log(1) });
 
+// 2:
+app.post('/:word', (req, res) => { console.log(2) });
+
+// 3:
+app.post('/hello', (req, res) => { console.log(3) });
+
+// 4:
+app.post('/:word', (req, res) => { console.log(4) });
 
 // DO NOT EDIT - Set port and listener
 const port = 5000;
